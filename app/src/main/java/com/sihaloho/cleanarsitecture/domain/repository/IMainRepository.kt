9caab9b8bf7b443d4f5b1dfed0728af607dba53c
sihaloho <madonaapry@gmail.com>
2021-04-08 20:24:12 +0700

    tidak sistem offline online

--- app/src/main/java/com/sihaloho/cleanarsitecture/domain/repository/IMainRepository.kt
+++ app/src/main/java/com/sihaloho/cleanarsitecture/domain/repository/IMainRepository.kt
@@ -1,11 +1,10 @@
 package com.sihaloho.cleanarsitecture.domain.repository
 
+import com.sihaloho.cleanarsitecture.data.Resource
 import com.sihaloho.cleanarsitecture.domain.entity.Game
 import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 
 interface IMainRepository {
-
-    fun getListGame(): Flow<List<Game>>
-
-//    fun getListGame() : Flow<ResponseApi<List<Game>>>
+    fun getListGame() : Flow<List<Game>>
 }
