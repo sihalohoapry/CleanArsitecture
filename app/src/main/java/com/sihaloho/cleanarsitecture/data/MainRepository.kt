7b0e5f5df078346845b8c552476d38db4fe7e080
sihaloho <madonaapry@gmail.com>
2021-04-10 17:04:55 +0700

    tidak sistem offline online

--- app/src/main/java/com/sihaloho/cleanarsitecture/data/MainRepository.kt
+++ app/src/main/java/com/sihaloho/cleanarsitecture/data/MainRepository.kt
@@ -1,7 +1,6 @@
 package com.sihaloho.cleanarsitecture.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import com.sihaloho.cleanarsitecture.data.source.remote.RemoteDataSource
 import com.sihaloho.cleanarsitecture.data.utils.DataMapper
 import com.sihaloho.cleanarsitecture.domain.entity.Game
@@ -12,13 +11,9 @@ import kotlinx.coroutines.flow.map
 
 class MainRepository(private val remoteDataSource: RemoteDataSource) : IMainRepository {
 
-    private var _isLoading = MutableLiveData<Boolean>()
-    private val isLoading : LiveData<Boolean> get() = _isLoading
 
     override fun getListGame(): Flow<List<Game>> {
-        _isLoading.value = true
         return remoteDataSource.getDataGame().map {
-            _isLoading.value = false
             DataMapper.responseToDomain(it)
         }
     }
